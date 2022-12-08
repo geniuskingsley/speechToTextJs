5e5e3e212e3ff3430115e2d508660e1e07d128b5
geniuskingsley <53303797+geniuskingsley@users.noreply.github.com>
2022-12-08 21:30:18 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+/**
+ *
+ * @Genius
+ */
+let click_to_convert = document.getElementById("click_to_convert");
+click_to_convert.addEventListener("click", function () {
+  let speech = true;
+  window.SpeechRecognition = window.webkitSpeechRecognition;
+  const recognition = new SpeechRecognition();
+  recognition.interimResults = true;
+
+  recognition.addEventListener("result", (e) => {
+    const transcript = Array.from(e.results)
+      .map((result) => result[0])
+      .map((result) => result.transcript);
+
+    convert_text.innerHTML = transcript;
+  });
+
+  if (speech == true) {
+    recognition.start();
+  }
+});
